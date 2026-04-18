bacca8abf8a51abd6c22f195e31490059f4f2b8d
Victor Shvets <f0ska@users.noreply.github.com>
2026-04-18 21:15:37 +0300

    grid event

--- src/Action/GridAction.php
+++ src/Action/GridAction.php
@@ -13,27 +13,33 @@ declare(strict_types=1);
 namespace F0ska\AutoGridBundle\Action;
 
 use F0ska\AutoGridBundle\Builder\GridQueryBuilder;
+use F0ska\AutoGridBundle\Event\GridEvent;
 use F0ska\AutoGridBundle\Model\AutoGrid;
 use F0ska\AutoGridBundle\Model\Parameters;
+use Symfony\Contracts\EventDispatcher\EventDispatcherInterface;
 
 class GridAction extends AbstractAction
 {
     private GridQueryBuilder $gridQueryBuilder;
+    private EventDispatcherInterface $dispatcher;
 
-    public function __construct(GridQueryBuilder $gridQueryBuilder)
+    public function __construct(GridQueryBuilder $gridQueryBuilder, EventDispatcherInterface $dispatcher)
     {
         $this->gridQueryBuilder = $gridQueryBuilder;
+        $this->dispatcher = $dispatcher;
     }
 
     public function execute(AutoGrid $autoGrid, Parameters $parameters): void
     {
         $count = (int) $this->gridQueryBuilder->buildGridCountQuery($parameters)->getSingleScalarResult();
         $parameters->initPagination($count);
-        $queryRows = $this->gridQueryBuilder->buildGridQuery($parameters);
-
+        $entities = $this->gridQueryBuilder->buildGridQuery($parameters)->getResult();
+        $event = new GridEvent($entities, $count, $parameters);
+        $this->dispatcher->dispatch($event, $event::EVENT_NAME);
+        $this->dispatcher->dispatch($event, $event::EVENT_NAME . '.' . $autoGrid->getId());
         $autoGrid->setTemplate($parameters->getActionTemplate('grid'));
         $autoGrid->setContext(
-            $parameters->render(['rows' => $queryRows->getResult()])
+            $parameters->render(['rows' => $entities])
         );
     }
 }
